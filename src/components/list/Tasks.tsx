011e55bd028e43bbb18680f416dc570982b26f04
Alessandro0003 <alessandroquinta3@gmail.com>
2024-03-27 06:36:11 -0300

    🔥 feat: add method remove tasks and method update cout tasks and check tasks

--- src/components/list/Tasks.tsx
+++ src/components/list/Tasks.tsx
@@ -1,25 +1,43 @@
+import { ITasks } from '../../App'
 import styles from './Tasks.module.css'
 import { Trash, Check } from '@phosphor-icons/react'
 
-export function Tasks() {
-  const checkboxCheckedClassname =
-    styles['checkbox-checked'] ?? styles['checkbox-unchecked']
-  const paragraphCheckedClassname = styles['paragraph-checked']
+interface TasksProps {
+  data: ITasks
+  removeTasks: (id: number) => void
+  toggleTasksStatus: ({ id, value }: { id: number; value: boolean }) => void
+}
+
+export function Tasks({ data, removeTasks, toggleTasksStatus }: TasksProps) {
+  function handleTasksToggle() {
+    toggleTasksStatus({ id: data.id, value: !data.isChecked })
+  }
+
+  function handleTasksRemove() {
+    removeTasks(data.id)
+  }
+
+  const checkboxCheckedClassname = data.isChecked
+    ? styles['checkbox-checked']
+    : styles['checkbox-unchecked']
+  const paragraphCheckedClassname = data.isChecked
+    ? styles['paragraph-checked']
+    : ''
 
   return (
     <div className={styles.container}>
       <div>
-        <label htmlFor="checkbox">
-          <input readOnly type="checkbox" />
+        <label htmlFor="checkbox" onClick={handleTasksToggle}>
+          <input readOnly type="checkbox" checked={data.isChecked} />
           <span className={`${styles.checkbox} ${checkboxCheckedClassname}`}>
-            <Check size={12} color="#ffff" />
+            {data.isChecked && <Check size={12} color="#ffff" />}
           </span>
-          <p
-            className={`${styles.paragraph}  ${paragraphCheckedClassname}`}
-          ></p>
+          <p className={`${styles.paragraph}  ${paragraphCheckedClassname}`}>
+            {data.text}
+          </p>
         </label>
       </div>
-      <button>
+      <button onClick={handleTasksRemove}>
         <Trash size={16} color="#808080" />
       </button>
     </div>
