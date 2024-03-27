16597e2ea3ceea5aaf454d71c56ec73193ed77a3
Alessandro0003 <alessandroquinta3@gmail.com>
2024-03-27 06:37:21 -0300

    🔥 feat: add attributes input add tasks

--- src/components/InputAddTasks.tsx
+++ src/components/InputAddTasks.tsx
@@ -1,12 +1,17 @@
 import styles from './InputAddTasks.module.css'
 
-export function InputAddTasks() {
+export function InputAddTasks({
+  ...rest
+}: React.DetailedHTMLProps<
+  React.InputHTMLAttributes<HTMLInputElement>,
+  HTMLInputElement
+>) {
   return (
     <div>
       <input
         className={styles.input}
-        type="text"
         placeholder="Adicione uma nova tarefa"
+        {...rest}
       />
     </div>
   )
