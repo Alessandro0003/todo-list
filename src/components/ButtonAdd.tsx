30aa2c2ca2eaba92974f5fc0e366fda2a822daab
Alessandro0003 <alessandroquinta3@gmail.com>
2024-03-27 06:36:48 -0300

    🔥 feat: add method attributes button

--- src/components/ButtonAdd.tsx
+++ src/components/ButtonAdd.tsx
@@ -2,11 +2,16 @@ import { PlusCircle } from '@phosphor-icons/react'
 
 import styles from './ButtonAdd.module.css'
 
-export function ButtonAdd() {
+type Props = React.DetailedHTMLProps<
+  React.ButtonHTMLAttributes<HTMLButtonElement>,
+  HTMLButtonElement
+>
+
+export function ButtonAdd({ children, ...rest }: Props) {
   return (
     <div>
-      <button className={styles.button}>
-        Criar
+      <button className={styles.button} {...rest}>
+        {children}
         <PlusCircle className={styles.iconPlus} size={18} />
       </button>
     </div>
